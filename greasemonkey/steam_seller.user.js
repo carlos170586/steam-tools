657e1d30075fede3d9246b736cbddd18544154d7
carlos170586 <web@4pelagatos.es>
2015-11-02 12:12:09 +0100

    add "send gifts" bulk option

--- greasemonkey/steam_seller.user.js
+++ greasemonkey/steam_seller.user.js
@@ -3,11 +3,38 @@
 // @namespace      https://github.com/carlos170586/steam-tools/
 // @description    Quickly sell items on steam market
 // @include        /http[s]?:\/\/[www]?steamcommunity.com\/id/[^\/]+/inventory.+/
-// @include        http://steamcommunity.com/profiles/*/inventory*
+// @include        https://store.steampowered.com/checkout/sendgift/*
 // @updateURL      https://github.com/carlos170586/steam-tools/raw/master/greasemonkey/steam_seller.user.js
-// @version        4
+// @version        5
 // @grant          none
 // ==/UserScript==
+
+
+function getCookie(name){
+  var parts = document.cookie.split(name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+function setCookie(cookieName,cookieValue,nDays){
+ var today = new Date();
+ var expire = new Date();
+ if (nDays==null || nDays==0) nDays=15;
+ expire.setTime(today.getTime() + 3600000*24*nDays);
+ document.cookie = cookieName+"="+escape(cookieValue)
+                 + ";expires="+expire.toGMTString();
+}
+
+function getQueryString(c){
+	if(!c)
+		c='?';//#
+	var r = {},
+		p = document.location.toString().split(c)[1].split('&');
+	for(var i in p){
+		var p2 = p[i].toString().split('=');
+		r[p2[0]]=p2[1];
+	}
+	return r;
+}
 
 Array.prototype.next = function() {
     if (!((this.current + 1) in this)) return false;
@@ -202,6 +229,60 @@ Array.prototype.current = 0;
 		});
 	}
 
+
+
+	function sendGift(giftTo,item){
+		if(!item)
+			return;
+		$J.ajax({
+			url:'https://store.steampowered.com/checkout/sendgiftsubmit/',
+			type:'POST',
+			data:{
+				GifteeAccountID:giftTo,
+				GifteeEmail:'',
+				GifteeName:'Monino Script',
+				GiftMessage:'Monino Script',
+				GiftSentiment:'Monino Script',
+				GiftSignature:'Monino Script',
+				GiftGID:item,
+				SessionID:g_sessionID
+			},
+			crossDomain:true,
+			xhrFields:{withCredentials:true}
+		}).always(function(d){
+			sendGift(giftTo,itemlist.next());
+		});
+	}
+
+
+	function redirect2Gifts(){
+		window.location='https://store.steampowered.com/checkout/sendgift/#To='+($J('#SteamGiftID').val().substr(3)-61197960265728)+'&Gifts='+itemlist.join(',');
+	}
+
+
+	function sendGifts(){
+		setCookie('GiftsUserID',$J('#SteamGiftID').val(),15);
+		clearLog('Checking...');
+		if($J('#SteamGiftID').val().length!=17)
+			return Log('Error: invalid user id');
+		clearLog('Send next items to <a href="https://steamcommunity.com/profiles/'+$J('#SteamGiftID').val()+'">https://steamcommunity.com/profiles/'+$J('#SteamGiftID').val()+'</a>');
+		
+		itemlist=[];
+		
+		var rgI=g_rgAppContextData[753].rgContexts[1].inventory.rgInventory;
+		for(var x in rgI){
+			if(!rgI[x].tradable && $J('#untradableGifts').is(':checked')
+			||	rgI[x].tradable && $J('#tradableGifts').is(':checked')){
+				itemlist.push(rgI[x].id);
+				Log(rgI[x].name);
+			}
+		}
+		if(itemlist.length>0)
+			$J('.confirm').show();
+	};
+
+
+
 	$J(document).ready(function(){
 
 		if($J('#global_actions a.user_avatar').attr('href')!=$J('.profile_small_header_bg .profile_small_header_name a').attr('href')+'/')
@@ -210,8 +291,8 @@ Array.prototype.current = 0;
 		$J('#inventory_applogo').after('<div id=SteamSeller style="line-height:45px;margin:10px">\
 			<label><input type="radio" name="sellType" value=0>Sell All</label>\
 			<label><input type="radio" name="sellType" value=1 checked>Sell Visibles</label>\
-			<!--<label><input type="radio" name="sellType" value=2>Convert All into Gems</label>-->\
-			<label><input type="radio" name="sellType" value=3>Convert Visibles into Gems</label>\
+			<label><input type="radio" name="sellType" value=2>Convert Visibles into Gems</label>\
+			<label><input type="radio" name="sellType" value=3>Send Gifts</label>\
 			\
 			<div id=SteamSellerLog style="line-height:normal"></div>\
 			<a class="btn_green_white_innerfade btn_medium_wide confirm" style="display:none"><span>Confirm</span></a>\
@@ -226,10 +307,13 @@ Array.prototype.current = 0;
 				<a class="btn_green_white_innerfade btn_medium_wide SteamSeller"><span>Sell visible items</span></a>\
 			</div>\
 			<div class="sellType s2">\
-				<a class="btn_green_white_innerfade btn_medium_wide SteamSeller"><span>Convert all cards into Gems</span></a>\
+				<a class="btn_green_white_innerfade btn_medium_wide SteamSeller"><span>Convert visible cards into Gems</span></a>\
 			</div>\
 			<div class="sellType s3">\
-				<a class="btn_green_white_innerfade btn_medium_wide SteamSeller"><span>Convert visible cards into Gems</span></a>\
+				<label title="Send Gifts tradables"><input type=checkbox id="tradableGifts">Send tradable Gifts</label><br>\
+				<label title="Send Gifts untradables"><input type=checkbox id="untradableGifts" checked>Send untradable Gifts</label><br>\
+				<label title="The User ID">User ID: <input type=text value="'+(getCookie('GiftsUserID')?getCookie('GiftsUserID'):'')+'" id="SteamGiftID"></label><br>\
+				<a class="btn_green_white_innerfade btn_medium_wide SteamSeller"><span>Send Gifts</span></a>\
 			</div>\
 		</div>');
 
@@ -240,17 +324,19 @@ Array.prototype.current = 0;
 			else if(o==1)
 				SellVisibles(GetPriceValueAsInt($J('#priceVisible').val()))
 			else if(o==2)
-				CalculateGems();
-			else if(o==3)
 				CalculateVisibleGems();
+			else if(o==3)
+				sendGifts();
 		});
 
 		$J('.confirm').click(function(){
 			var o=$J('input[name="sellType"]:checked').val();
 			if(o==0 || o==1)
 				sellItem(itemlist[0]);
-			if(o==2 || o==3)
+			if(o==2)
 				convertIntoGems(itemlist[0]);
+			if(o==3)
+				redirect2Gifts();
 			$J('.confirm').hide();
 		});
 
@@ -260,4 +346,8 @@ Array.prototype.current = 0;
 			$J('.sellType.s'+$J('input[name="sellType"]:checked').val()).show();
 		}).change();
 	});
+	if(location.host + location.pathname=='store.steampowered.com/checkout/sendgift/'){
+		itemlist=getQueryString('#')['Gifts'].split(',');
+		sendGift(getQueryString('#')['To'],itemlist[0]);
+	}
 })();
