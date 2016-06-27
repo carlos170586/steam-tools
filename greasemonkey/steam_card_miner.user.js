13f9e30a8bc94b9b45c7b805659ddc585e957152
carlos170586 <web@4pelagatos.es>
2016-06-28 01:49:59 +0200

    Update steam_card_miner.user.js

--- greasemonkey/steam_card_miner.user.js
+++ greasemonkey/steam_card_miner.user.js
@@ -2,6 +2,7 @@
 // @name        Steam Card Miner
 // @namespace   steamcardminer
 // @include     *//store.steampowered.com/explore/
+// @include     *//store.steampowered.com/agecheck/*
 // @include     *//store.steampowered.com/app/*
 // @version     1
 // @grant       none
@@ -14,6 +15,10 @@ this script auto click in the game pages to get all cards
 
 $J(document).ready(function(){
 	$J('#refresh_queue_btn').click();
-	$J('.btn_grey_white_innerfade.btn_medium').click();
 	$J('.btn_next_in_queue').click();
+	
+	if(location.href.indexOf('agecheck')>-1){
+		$J('.btn_grey_white_innerfade.btn_medium').click();
+		$J('.btnv6_blue_hoverfade.btn_small').click();
+	}
 });
